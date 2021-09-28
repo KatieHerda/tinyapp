eacfb505dea549efc9c86b94728c1382de73d158
Katie <katie.herda2@gmail.com>
2021-09-28 15:52:08 +0000

    added comments to better understand the flow of each component.

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+//tells express app to use EJS as its templating engine
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -9,6 +10,8 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+// when browser submits a post request, the data in body is sent as buffer, not readable 
+// a body parser library will convert the request body from buffer into readable string
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,21 +20,30 @@ const generateRandomString = () => {
   return Math.random().toString(36).substr(2, 6);
 };
 
+//recieves form submission and creates a new key:value pair in obj
+//shortURL:longUR
+//redirected to shortURL section
 app.post('/urls', (req, res) => {
   let generatedShortURL = generateRandomString();
   urlDatabase[generatedShortURL] = req.body.longURL;
   res.redirect(`/urls/${generatedShortURL}`);
 });
 
+//renders the urls_new template in browser, presents the form to the user.
+//needs to be before the get /urls/:id
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// : indicates that shortURL is a route paramater
+//the value in this part of the URL will be available in the req.params obj
+//both the shortURL and longURL are passed to the template in a templateVars obj
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 });
 
+//when the shortURL is clicked on, it redirects to actual website.
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
 
@@ -50,19 +62,23 @@ app.get('/u/:shortURL', (req, res) => {
 
 });
 
+//urls route that uses res.render to pass URL data to the template
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+//Root path
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+//JSON string representing entire urlDatabase obj
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+//Repsponse that contains HTML code, rendered in client browser
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
